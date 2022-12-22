efd94eca62a4d67420fccbe835f4ace01f366c55
Orudnev <olrudnev@gmail.com>
2022-12-22 11:53:47 +0600

    GUI refactoring: "Config" button added, buttons "MP3","EN","RU" ,"Pause duration" moved to config page, config settings serialization/deserialization is implemented via localstorage

--- src/components/SPlayer.tsx
+++ src/components/SPlayer.tsx
@@ -9,6 +9,19 @@ enum langEnum {
     ruRu = "ru=RU"
 }
 
+enum lstorageKey{
+    config="config",
+    results="results"
+}
+
+interface IConfigSettings{
+    mp3Enabled:boolean;
+    enEnabled: boolean;
+    ruEnabled: boolean;
+    pause: number;
+}
+
+
 interface ISPlayerState {
     file?: File
     url?: string,
@@ -18,12 +31,11 @@ interface ISPlayerState {
     isSayBtnPlaying: boolean,
     lang: langEnum,
     sayText: string,
+    isStarted: boolean;
     isPaused: boolean;
-    mp3Enabled: boolean;
-    enEnabled: boolean;
-    ruEnabled: boolean;
-    pause: number;
-    MoveNextItemAutomatically:boolean;
+    configPaneVisibility: boolean;
+    configSettings:IConfigSettings;
+    MoveNextItemAutomatically: boolean;
     items: any[]
 }
 
@@ -63,12 +75,16 @@ const PauseTuner = (props: IPauseTunerProps) => {
         let middleX = elmRect.left + elmRect.width / 2;
         let state = props.parent.state;
         let touchX = e.touches[0].clientX;
+        let updateConfig = (updProps:any)=>{
+            let newConfig = {...state.configSettings,...updProps};
+            props.parent.setState({configSettings:newConfig});
+        };
         if (touchX > middleX) {
             //right button clicked
-            props.parent.setState({ pause: state.pause + 1 });
+            updateConfig({ pause: state.configSettings.pause + 1 });
         } else {
             //left button clicked
-            props.parent.setState({ pause: state.pause - 1 });
+            updateConfig({ pause: state.configSettings.pause - 1 });
         }
         if (props.parent.touchState > 0) {
             setTimeout(() => changePauseValue(e), 100);
@@ -81,10 +97,11 @@ const PauseTuner = (props: IPauseTunerProps) => {
             handleMouseDown(e);
         }, 100)}>
             <div id="label" >pause</div>
-            <div id="value">{props.parent.state.pause}</div>
+            <div id="value">{props.parent.state.configSettings.pause}</div>
         </div>
     );
 }
+
 
 export class SPlayer extends React.Component<any, ISPlayerState> {
     aref: React.RefObject<HTMLAudioElement>;
@@ -101,12 +118,16 @@ export class SPlayer extends React.Component<any, ISPlayerState> {
             endTime: 15,
             lang: langEnum.ruRu,
             sayText: "",
+            isStarted: false,
             isPaused: false,
-            mp3Enabled: true,
-            enEnabled: true,
-            ruEnabled: true,
-            pause: 50, //unit: 0.1 second 
-            MoveNextItemAutomatically:true,
+            configPaneVisibility: false,
+            configSettings:{
+                mp3Enabled: true,
+                enEnabled: true,
+                ruEnabled: true,
+                pause: 50, //unit: 0.1 second     
+            },
+            MoveNextItemAutomatically: true,
             items: []
         }
         this.aref = createRef();
@@ -128,6 +149,11 @@ export class SPlayer extends React.Component<any, ISPlayerState> {
         binToStringConverter.addEventListener("loadend", this.onBinToStringConverted);
         window.addEventListener("touchstart", this.onTouchStart);
         window.addEventListener("touchend", this.onTouchEnd);
+        let configStr = localStorage.getItem(lstorageKey.config);
+        if(configStr){
+            let config = JSON.parse(configStr);
+            this.setState({configSettings:config});   
+        }
     }
 
     onTouchStart() {
@@ -155,12 +181,22 @@ export class SPlayer extends React.Component<any, ISPlayerState> {
     loadFile(selFile: File) {
         JSZip.loadAsync(selFile as Blob)
             .then((zip) => {
-                zip.files["Introduction.json"].async('blob').then((res: any) => {
-                    binToStringConverter.readAsText(res);
-                });
-                zip.files["Introduction.mp3"].async('blob').then((res: any) => {
-                    this.setState({ file: res, url: URL.createObjectURL(res as Blob) })
-                });
+                let fileNames = [];
+                for (let prop in zip.files) {
+                    fileNames.push(prop);
+                }
+                let jsonFileName = fileNames.find(itm => itm.includes(".json"));
+                let mp3FileName = fileNames.find(itm => itm.includes(".mp3"));
+                if (jsonFileName) {
+                    zip.files[jsonFileName].async('blob').then((res: any) => {
+                        binToStringConverter.readAsText(res);
+                    });
+                }
+                if (mp3FileName) {
+                    zip.files[mp3FileName].async('blob').then((res: any) => {
+                        this.setState({ file: res, url: URL.createObjectURL(res as Blob) })
+                    });
+                }
             })
 
         // this.setState({ file: selFile, url: URL.createObjectURL(selFile as Blob) })
@@ -170,7 +206,25 @@ export class SPlayer extends React.Component<any, ISPlayerState> {
         this.setState({ isPaused: !this.state.isPaused });
     }
 
-    async handleStartButtonClick() {
+    handleConfigButtonClick() {
+        this.setState({ configPaneVisibility: !this.state.configPaneVisibility });
+    }
+
+    handleExitButtonClick(){
+        this.handleConfigButtonClick();
+        localStorage.setItem(lstorageKey.config,JSON.stringify(this.state.configSettings));
+    }
+
+    handleStartButtonClick() {
+        this.setState({ isStarted: !this.state.isStarted }, () => {
+            if (this.state.isStarted) {
+                this.startPlaySpeech()
+            }
+        });
+    }
+
+
+    async startPlaySpeech() {
         async function wait(ms = 200) {
             return new Promise(resolve => {
                 setTimeout(resolve, ms)
@@ -181,18 +235,18 @@ export class SPlayer extends React.Component<any, ISPlayerState> {
                 await wait();
             }
         }
-        for (let i: number = 0; i < this.state.items.length; ) {
+        for (let i: number = 0; i < this.state.items.length;) {
             let currItem = this.state.items[i];
             this.setState({ startTime: currItem.startTime, endTime: currItem.endTime, isMP3Playing: true, sayText: currItem.en }, () => {
                 this.playMP3Fragment()
             });
-            if (this.state.mp3Enabled) {
+            if (this.state.isStarted && this.state.configSettings.mp3Enabled && currItem.file) {
                 await waitWhile(() => this.state.isMP3Playing === false); //wait state.isPlaying == true (MP3 fragment playing start)
                 await waitWhile(() => this.state.isMP3Playing === true); //wait state.isPlaying == false (MP3 fragment playing end)
                 await waitWhile(() => this.state.isPaused); //wait switching to this.state.isPaused == false   
             }
 
-            if (this.state.enEnabled) {
+            if (this.state.isStarted && this.state.configSettings.enEnabled && currItem.en) {
                 this.setState({ lang: langEnum.enUs, sayText: currItem.en, isSayBtnPlaying: true });
                 await waitWhile(() => this.state.isSayBtnPlaying === false); //wait state.isSayBtnPlaying == true (Say button fragment playing start)            
                 this.playSayButton();
@@ -200,23 +254,26 @@ export class SPlayer extends React.Component<any, ISPlayerState> {
                 await waitWhile(() => this.state.isPaused); //wait switching to this.state.isPaused == false    
             }
 
-            if (this.state.ruEnabled) {
+            if (this.state.isStarted && this.state.configSettings.ruEnabled && currItem.ru) {
                 this.setState({ lang: langEnum.ruRu, sayText: currItem.ru, isSayBtnPlaying: true });
                 await waitWhile(() => this.state.isSayBtnPlaying === false); //wait state.isSayBtnPlaying == true (Say button fragment playing start)            
                 this.playSayButton();
                 await waitWhile(() => this.state.isSayBtnPlaying === true); //wait state.isSayBtnPlaying == false (Say button fragment playing end)  
                 await waitWhile(() => this.state.isPaused); //wait switching to this.state.isPaused == false    
             }
-            await wait(this.state.pause*100);
-            if(this.state.MoveNextItemAutomatically){
+            if (!this.state.isStarted) {
+                break;
+            }
+            await wait(this.state.configSettings.pause * 100);
+            if (this.state.MoveNextItemAutomatically) {
                 i++;
             }
         }
     }
 
     playMP3Fragment() {
-        if (this.state.mp3Enabled) {
-            if (this.aref.current) {
+        if (this.state.configSettings.mp3Enabled) {
+            if (this.aref.current && this.state.file) {
                 this.aref.current.currentTime = this.state.startTime;
                 this.aref.current.play();
             }
@@ -247,39 +304,59 @@ export class SPlayer extends React.Component<any, ISPlayerState> {
         );
     }
 
-    render(): React.ReactNode {
-        if (!this.state.file) {
-            return this.renderLoadFile();
-        }
+
+    renderConfigPane() {
+        let mp3ButtonClassStr = (this.state.configSettings.mp3Enabled ? "toolbar-button toolbar-button__text toolbar-button__enabled" : "toolbar-button toolbar-button__text toolbar-button__disabled");
+        let enButtonClassStr = (this.state.configSettings.enEnabled ? "toolbar-button toolbar-button__text toolbar-button__enabled" : "toolbar-button toolbar-button__text toolbar-button__disabled");
+        let ruButtonClassStr = (this.state.configSettings.ruEnabled ? "toolbar-button toolbar-button__text toolbar-button__enabled" : "toolbar-button toolbar-button__text toolbar-button__disabled");
+        let updateConfig = (updProps:any)=>{
+            let newConfig = {...this.state.configSettings,...updProps};
+            this.setState({configSettings:newConfig});
+        };
+        return (
+            <div className="splayer-page"> 
+                <button className="toolbar-button " style={{marginBottom:"10px"}} onClick={() => {
+                    this.handleExitButtonClick();}}>
+                    <div className="img-exit" />
+                </button>
+                <div className="splayer-page__toolbar">
+                    <button className={mp3ButtonClassStr} onClick={() => {
+                        updateConfig({ mp3Enabled: !this.state.configSettings.mp3Enabled });
+                    }}>MP3</button>
+                    <button className={enButtonClassStr} onClick={() => {
+                        updateConfig({ enEnabled: !this.state.configSettings.enEnabled });
+                    }}>EN</button>
+                    <button className={ruButtonClassStr} onClick={() => {
+                        updateConfig({ ruEnabled: !this.state.configSettings.ruEnabled });
+                    }}>RU</button>
+                    <PauseTuner parent={this} />
+                </div>
+            </div>
+        );
+    }
+
+    renderMainPane() {
+        let startButtonClassStr = (this.state.isStarted ? "toolbar-button toolbar-button__enabled" : "toolbar-button toolbar-button__disabled");
         let pauseButtonClassStr = (this.state.isPaused ? "img-play" : "img-pause");
-        let mp3ButtonClassStr = (this.state.mp3Enabled ? "toolbar-button toolbar-button__text toolbar-button__enabled" : "toolbar-button toolbar-button__text toolbar-button__disabled")
-        let enButtonClassStr = (this.state.enEnabled ? "toolbar-button toolbar-button__text toolbar-button__enabled" : "toolbar-button toolbar-button__text toolbar-button__disabled")
-        let ruButtonClassStr = (this.state.ruEnabled ? "toolbar-button toolbar-button__text toolbar-button__enabled" : "toolbar-button toolbar-button__text toolbar-button__disabled")
-        let moveNextItemClassStr = (this.state.MoveNextItemAutomatically ? "toolbar-button toolbar-button__text toolbar-button__enabled" : "toolbar-button toolbar-button__text toolbar-button__disabled")
+        let moveNextItemClassStr = (this.state.MoveNextItemAutomatically ? "toolbar-button toolbar-button__text toolbar-button__enabled" : "toolbar-button toolbar-button__text toolbar-button__disabled");
+
         return (
             <div className="splayer-page">
                 <div className="splayer-page__toolbar">
-                    <button className="toolbar-button" onClick={() => {
+                    <button className={startButtonClassStr} onClick={() => {
                         this.handleStartButtonClick();
                     }}><div className="img-power-on" />
                     </button>
                     <button className='toolbar-button' onClick={() => {
                         this.handlePauseButtonClick();
                     }}><div className={pauseButtonClassStr} /></button>
-                    <button className={mp3ButtonClassStr} onClick={() => {
-                        this.setState({ mp3Enabled: !this.state.mp3Enabled });
-                    }}>MP3</button>
-                    <button className={enButtonClassStr} onClick={() => {
-                        this.setState({ enEnabled: !this.state.enEnabled });
-                    }}>EN</button>
-                    <button className={ruButtonClassStr} onClick={() => {
-                        this.setState({ ruEnabled: !this.state.ruEnabled });
-                    }}>RU</button>
-                    <PauseTuner parent={this} />
                     <button className={moveNextItemClassStr} onClick={() => {
-                        this.setState({ MoveNextItemAutomatically: !this.state.MoveNextItemAutomatically});
+                        this.setState({ MoveNextItemAutomatically: !this.state.MoveNextItemAutomatically });
                     }}>next</button>
-
+                    <button className={"toolbar-button"} onClick={() => {
+                        this.handleConfigButtonClick();
+                    }}><div className="img-config" />
+                    </button>
                 </div>
 
                 <audio ref={this.aref} src={this.state.url} loop={true} onTimeUpdate={(e) => {
@@ -303,5 +380,15 @@ export class SPlayer extends React.Component<any, ISPlayerState> {
                 <div className="splayer_page__text">{this.state.sayText}</div>
             </div>
         );
+    }
+
+    render(): React.ReactNode {
+        if (!this.state.file && this.state.items.length == 0) {
+            return this.renderLoadFile();
+        }
+        if (this.state.configPaneVisibility) {
+            return this.renderConfigPane();
+        }
+        return this.renderMainPane();
     }
 }
